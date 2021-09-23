24d042f7ca43a03209d98b4fc4e5771028bda7a3
Mathieu Marchand <90476463+MathieuMarchandFR@users.noreply.github.com>
2021-09-23 22:25:46 +0200

    Add files via upload

---
+++ links.py
@@ -0,0 +1,51 @@
+import os
+import csv
+import requests
+from bs4 import BeautifulSoup
+import codecs 
+
+def encodageMaison(texte):
+    AccentMoche = ['Ã©', 'Ã¨', 'Ã ', 'Ã¯', 'Ã´', 'Ã§', 'Ãª', 'Ã«', 'Ã¢', 'â™', 'â™€', 'Ã‰', 'ï»¿']
+    AccentBeau = ['é', 'è', 'à', 'ï', 'ô', 'ç', 'ê', 'ë', 'â', '♂', '♀', 'É', '']
+    for i in range(len(AccentMoche)):
+        texte = texte.replace(AccentMoche[i],AccentBeau[i])
+    return texte
+
+
+file = open('C:\\Users\\33606\\Documents\\JDR\\jdr Pokemon\\Livre de Base Beta\\Data\\Build Fiches opti\\Scrapper\\nomPokemon.csv',newline='')
+reader = csv.reader(file)
+
+
+links = []
+for row in reader:
+    row = str(row)
+    row = row[2:]
+    row = row[:-2]
+    links.append('https://www.pokepedia.fr/' + row)
+file.close
+
+
+with open('urls.txt', 'w') as file:
+    for link in links:
+        file.write(link + '\n')
+
+
+with open('urls.txt', 'r') as file:
+    with open('extract.csv', 'w') as outf:
+        outf.write('extract\n')
+        for row in file:
+            url = str(row.strip())
+            url = encodageMaison(url)
+            response = requests.get(url)
+            
+
+            if response.ok:
+                soup = BeautifulSoup(response.text,'lxml')
+                if soup.find('tbody', {'class': 'ListeCapacites8'}) is None:
+                    #extract = soup.find('tbody', {'class': 'ListeCapacites7'}).findAll('a', title=True)
+                    extract = soup.find('tbody', {'class': 'ListeCapacites7'})
+                    outf.write(str(extract) + '\n')
+                else:
+                    #extract = soup.find('tbody', {'class': 'ListeCapacites8'}).findAll('a', title=True)
+                    extract = soup.find('tbody', {'class': 'ListeCapacites8'})
+                    outf.write(str(extract) + '\n')
