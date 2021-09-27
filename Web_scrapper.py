f6233de42930a401e2c624dc5544e83529c24b29
Mathieu Marchand <90476463+MathieuMarchandFR@users.noreply.github.com>
2021-09-27 12:16:28 +0200

    Update and rename links.py to Web_scrapper.py

--- Web_scrapper.py
+++ Web_scrapper.py
@@ -1,6 +1,6 @@
-#Ce scraper a pour but de récupérer les attaques d'une liste de Pokémon sur le site Poképédia
-#Les résultats n'ont pas été très concluant et je n'avais finalement pas besoin de ces données
-#J'ai néanmoins appris les bases du scrapping avec BeautifulSoup et je pourrais m'adapter à d'autres sites et pousser mes connaissances si besoin.
+#This scraper aims to retrieve attacks from a list of Pokémon on the Poképédia website
+#The results were not very conclusive and I didn't need this data
+#I learned the basics of scrapping with BeautifulSoup and I could adapt to other sites and push my knowledge if needed.
 
 import os
 import csv
@@ -8,12 +8,12 @@ import requests
 from bs4 import BeautifulSoup
 import codecs 
 
-def encodageMaison(texte):
-    AccentMoche = ['Ã©', 'Ã¨', 'Ã ', 'Ã¯', 'Ã´', 'Ã§', 'Ãª', 'Ã«', 'Ã¢', 'â™', 'â™€', 'Ã‰', 'ï»¿']
-    AccentBeau = ['é', 'è', 'à', 'ï', 'ô', 'ç', 'ê', 'ë', 'â', '♂', '♀', 'É', '']
+def Homemade_encoding(text):
+    raw_special_character = ['Ã©', 'Ã¨', 'Ã ', 'Ã¯', 'Ã´', 'Ã§', 'Ãª', 'Ã«', 'Ã¢', 'â™', 'â™€', 'Ã‰', 'ï»¿']
+    clean_special_character = ['é', 'è', 'à', 'ï', 'ô', 'ç', 'ê', 'ë', 'â', '♂', '♀', 'É', '']
     for i in range(len(AccentMoche)):
         texte = texte.replace(AccentMoche[i],AccentBeau[i])
-    return texte
+    return text
 
 
 file = open('C:\\Users\\33606\\Documents\\JDR\\jdr Pokemon\\Livre de Base Beta\\Data\\Build Fiches opti\\Scrapper\\nomPokemon.csv',newline='')
@@ -39,7 +39,7 @@ with open('urls.txt', 'r') as file:
         outf.write('extract\n')
         for row in file:
             url = str(row.strip())
-            url = encodageMaison(url)
+            url = Homemade_encoding(url)
             response = requests.get(url)
             
 
